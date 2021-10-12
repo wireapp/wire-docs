67b87a47fe26e18ca4e49db5608f253df7b114a2
Stefan Matting <stefan@wire.com>
2021-10-12 10:51:05 +0200

    Change theme to readthedocs

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gnumake
-    (python3.withPackages (ps: with ps; [ sphinx recommonmark awscli rst2pdf sphinxcontrib-fulltoc ]))
+    (python3.withPackages (ps: with ps; [ sphinx recommonmark awscli rst2pdf sphinxcontrib-fulltoc sphinx_rtd_theme ]))
   ];
 }
